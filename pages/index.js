ecb8cab5e5652a133616692b6187144db59da98f
abhishek <abhishek.kumar30@oyorooms.com>
2021-01-17 01:19:39 +0530

    check

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
 import { Button, Alert } from "reactstrap";
+import fetch from 'isomorphic-unfetch';
+import axios from 'axios';
+import { useQuery } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
 
-export default () => {  
-  return (
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
+// const API_URL = "http://localhost:1337"
+
+const QUERY = gql`
+  {
+    blogs {
+      id
+      title
+      content
+    }
+  }
+`;
+
+// export async function getServerSideProps(){
+//   const response = await axios.get('http://localhost:1337/blogs');
+//   console.log(response);
+
+//   return {
+//     props: {
+//       data: response
+//     }
+//   }
+// }
+
+
+
+
+const Home = ({ restaurants, error }) => {
+  console.log(restaurants)
+  console.log(API_URL)
+  return(
     <div>
       <div>
         <Alert color="primary">
@@ -9,6 +42,25 @@ export default () => {
         </Alert>
         &nbsp; <Button color="primary">Hello from nextjs</Button>
       </div>
+      <ul>
+      {restaurants.map(restaurant => (
+        <li key={restaurant.id}>{restaurant.title}
+          <div>{restaurant.content}</div>
+        </li>
+      ))}
+    </ul>
     </div>
   );
+}
+
+Home.getInitialProps = async ctx => {
+  try {
+    const res = await axios.get(`${API_URL}/blogs`);
+    const restaurants = res.data;
+    return { restaurants };
+  } catch (error) {
+    return { error };
+  }
 };
+
+export default Home;
