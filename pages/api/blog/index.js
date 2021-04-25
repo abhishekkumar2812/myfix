394546835686b73b27e6e298f0aa675655a7ccb6
abhishekkumar2812 <vibrant.abhishek@gmail.com>
2021-04-26 02:59:08 +0530

    fix

--- pages/api/blog/index.js
+++ pages/api/blog/index.js
@@ -1,15 +1,15 @@
-import {supabase} from '../../utils/supabaseClient'
+// import {supabase} from '../../utils/supabaseClient'
 
 
-// Example of how to verify and get user data server-side.
-const getData = async (req, res) => {
-  //const token = req.headers.token;
+// // Example of how to verify and get user data server-side.
+// const getData = async (req, res) => {
+//   //const token = req.headers.token;
 
-  //const { data: user, error } = await supabase.auth.api.getUser(token);
+//   //const { data: user, error } = await supabase.auth.api.getUser(token);
 
-  //if (error) return res.status(401).json({ error: error.message });
-  //return res.status(200).json(user);
+//   //if (error) return res.status(401).json({ error: error.message });
+//   //return res.status(200).json(user);
   
-};
+// };
 
-export default getUser;
+// export default getUser;
